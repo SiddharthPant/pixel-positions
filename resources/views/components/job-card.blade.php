055bb994323335efd6d20d4959751f90aba35ee8
SiddharthPant <spsiddharthpant@gmail.com>
2024-06-29 14:05:46 +0530

    Make tags smaller and separate out panel component

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,8 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
@@ -10,6 +10,6 @@
             <x-tag>Tag</x-tag>
             <x-tag>Tag</x-tag>
         </div>
-        <img src="https://placehold.it/42/42" alt="Employer logo" class="rounded-2xl">
+        <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>
